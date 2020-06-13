2e2c24e5552276983fa95c8bd3c85bf279ac4fe2
tjjaramillo <58151139+tjjaramillo@users.noreply.github.com>
2020-06-13 11:54:06 -0500

    legend title

--- static/js/logic2.js
+++ static/js/logic2.js
@@ -123,7 +123,7 @@ function buildMap() {
   legend.onAdd = function (map) {
     var div = L.DomUtil.create("div", "info legend"),
       grades = [0, 300, 400, 500, 600, 700, 800, 900],
-      labels = [],
+      labels = ["<strong> Reports per 100K </strong>"],
       from,
       to;
 
